3639ca37b715b3098a76b41f6d7af41c046f8df2
ilya.mi@digitrade.co.il <ilya.mi@digitrade.co.il>
2024-08-08 17:04:53 +0300

    add

--- src/app.module.ts
+++ src/app.module.ts
@@ -70,7 +70,9 @@ import { ShieldModule } from './shield/shield.module';
           database:configService.get('DB_DATABASE'),
           autoLoadEntities:true,
           synchronize:true,
-        
+          ssl: {
+            rejectUnauthorized: false,  // This allows self-signed certificates
+          },
         }
       }
     }),
